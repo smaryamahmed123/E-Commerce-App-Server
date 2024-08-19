282958ec13d1db26fbe7c6b19284efe21868f669
Syeda Maryam Ahmed <smaryamahmed2006@gmail.com>
2024-08-19 11:27:51 +0500

    commit

--- models/User.js
+++ models/User.js
@@ -7,7 +7,7 @@ const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: {
     type: String,
-    required: [true, 'Password is required'], // Make the field required
+    // required: [true, 'Password is required'], // Make the field required
     minlength: [6, 'Password must be at least 6 characters long'], // Minimum length
     validate: {
       validator: function(v) {
